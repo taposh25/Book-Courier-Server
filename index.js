81c3b220471b15d82a4ce68b11a663e0c11f1708
Taposh Barai <taposhbarai03@gmail.com>
2025-12-10 11:55:45 +0600

    books-ali-added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const cors = requre('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 3000
@@ -7,6 +8,84 @@ const port = process.env.PORT || 3000
 //middleware
 app.use(express.json());
 app.use(cors());
+
+//connection string  user: book_courier_user, pass: W4dnLwEgc5RjHWSI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aa4hy5v.mongodb.net/?appName=Cluster0`;
+
+
+        // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+        const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+        });
+
+        
+        async function run() {
+        try {
+            // Connect the client to the server	(optional starting in v4.7)
+            await client.connect();
+
+            const db = client.db('book_courier_db');
+            const booksCollection = db.collection('books');
+
+            // books API
+
+           app.get('/books', async (req, res) => {
+            const query = {}
+      
+            const options = { sort: { createdAt: -1 } }
+
+            const cursor = booksCollection.find(query, options);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+            app.post('/books', async(req, res)=>{
+          const book = req.body;
+          book.createdAt = new Date()   
+           // Check for duplicate by title (or title + author)
+          const existingBook = await booksCollection.findOne({
+            title: book.title,
+            author: book.author
+          });
+
+          if (existingBook) {
+            return res.status(400).send({ error: "This book already exists!" });
+          }
+          const result = await booksCollection.insertOne(book);
+          res.send(result);
+      })
+
+
+        app.delete('/books/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await booksCollection.deleteOne(query);
+        if (result.deletedCount > 0) {
+          res.send({ success: true, message: "Book deleted successfully" });
+        } else {
+          res.send({ success: false, message: "Book not found" });
+        }
+      });
+
+
+
+
+            // Send a ping to confirm a successful connection
+            await client.db("admin").command({ ping: 1 });
+            console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        } finally {
+            // Ensures that the client will close when you finish/error
+            // await client.close();
+        }
+        }
+        run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Book Courier Service!')
